685a4371642485fb960e5b91f13b86303111264d
ffilloux <fillouxflorian56@mail.com>
2025-05-02 12:23:56 +0200

    fix : npm run build probleme dans l'ajout de project

--- src/app/api/admin/project/route.ts
+++ src/app/api/admin/project/route.ts
@@ -1,19 +1,20 @@
 // src/app/api/admin/project/route.ts
 import { NextResponse } from 'next/server';
-import {prisma} from '@/lib/prisma';
+import { prisma } from '@/lib/prisma';
 
 export async function POST(request: Request) {
-  const { title, date, skills, desc, jobTitle, link } = await request.json();
+  const { title, date, skills, desc, link, screens, whichCase } = await request.json();
 
   try {
     const newProject = await prisma.project.create({
       data: {
         title,
         date,
-        skills: JSON.parse(skills),
+        skills: Array.isArray(skills) ? skills : JSON.parse(skills),
+        screens: Array.isArray(screens) ? screens : JSON.parse(screens),
         desc,
-        jobTitle,
         link,
+        whichCase,
       },
     });
     return NextResponse.json(newProject, { status: 201 });
